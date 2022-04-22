170a5dc8f00fb32c3ff6895e1012be1c02e850e5
Andrew Ibrahim <androwsamir2019@gmail.com>
2022-04-22 02:11:15 +0200

    Send Message

--- UserForm.h
+++ UserForm.h
@@ -1,11 +1,11 @@
-
 #pragma once
+
 #include<cliext/map>
 #include<cliext/vector>
 #include<cliext/queue>
 #include<cliext/stack>
 #using <system.dll>
-/* fdsfsadfsdfsd */
+
 namespace BlueSara7a {
 
 	using namespace System;
@@ -27,16 +27,16 @@ namespace BlueSara7a {
 	private:
 		int i = 1;
 		int x = 60;
+		String^ name;
 		Form^ mainform;
 		long long ID = 0;
 		map<String^, long long>Contacts;
 		String^ username;
 		String^ password;
 		stack<String^> Messages;
-	private: System::Windows::Forms::ListBox^ Chat;
-
-		   queue<String^> favoritemessage;
-		   void set_Messages_in_file() {
+		queue<String^> LifeChat;
+		queue<String^> favoritemessage;
+		void set_Messages_in_file() {
 			   set_message_size();
 			   String^ filename = "Data/";
 			   filename += username + "_" + System::Convert::ToString(ID) + "/message.txt";
@@ -47,7 +47,7 @@ namespace BlueSara7a {
 			   }
 			   sw->Close();
 		   }
-		   void set_FavoriteMessages_in_file() {
+		void set_FavoriteMessages_in_file() {
 			   set_favoritemessage_size();
 			   String^ filename = "Data/";
 			   filename += username + "_" + System::Convert::ToString(ID) + "/favoritemessage.txt";
@@ -58,7 +58,7 @@ namespace BlueSara7a {
 			   }
 			   sw->Close();
 		   }
-		   void set_favoritemessage_size() {
+		void set_favoritemessage_size() {
 			   queue<String^>temp;
 			   //Console::WriteLine(favoritemessage.size());
 			   while (!favoritemessage.empty()) {
@@ -72,7 +72,7 @@ namespace BlueSara7a {
 				   temp.pop();
 			   }
 		   }
-		   void set_message_size() {
+		void set_message_size() {
 			   stack<String^>temp;
 			   //Console::WriteLine(Messages.size());
 			   while (!Messages.empty()) {
@@ -86,7 +86,21 @@ namespace BlueSara7a {
 				   temp.pop();
 			   }
 		   }
-		   void load_messages() {
+		void set_message_size2() {
+			   queue<String^>temp;
+			   //Console::WriteLine(Messages.size());
+			   while (!LifeChat.empty()) {
+				   if (LifeChat.front() != nullptr) {
+					   temp.push(LifeChat.front());
+					   LifeChat.pop();
+				   }
+			   }
+			   while (!temp.empty()) {
+				   LifeChat.push(temp.front());
+				   temp.pop();
+			   }
+		   }
+		void load_messages() {
 			   String^ filename = "Data/";
 			   filename += username + "_" + System::Convert::ToString(ID) + "/message.txt";
 			   try {
@@ -107,7 +121,29 @@ namespace BlueSara7a {
 			   }
 			   set_message_size();
 		   }
-		   void load_favorite() {
+		void load_messages(long long id) {
+			   String^ filename = "Data/";
+			   filename += username + "_" + System::Convert::ToString(ID) + "/" + System::Convert::ToString(id) + ".txt";
+			   try {
+				   Console::WriteLine("tying to open file", filename);
+				   StreamReader^ din = File::OpenText(filename);
+				   String^ str;
+				   while ((str = din->ReadLine()) != nullptr)
+				   {
+					   LifeChat.push(str);
+				   }
+				   din->Close();
+			   }
+			   catch (Exception^ e) {
+				   if (dynamic_cast<FileNotFoundException^>(e))
+					   Console::WriteLine("file '{0}' not found", filename);
+				   else
+					   Console::WriteLine("problem reading file '{0}'", filename);
+			   }
+			   set_message_size2();
+			   Display_Chat();
+		}
+		void load_favorite() {
 			   String^ filename = "Data/";
 			   filename += username + "_" + System::Convert::ToString(ID) + "/favoritemessage.txt";
 			   try {
@@ -128,7 +164,7 @@ namespace BlueSara7a {
 			   }
 			   set_favoritemessage_size();
 		   }
-		   void load_contacts() {
+		void load_contacts() {
 			   String^ filename = "Data/";
 			   filename += username + "_" + System::Convert::ToString(ID) + "/contacts.txt";
 			   try {
@@ -172,19 +208,19 @@ namespace BlueSara7a {
 			   }
 			   Display_Contacts();
 		   }
-		   void Undo_last_message() {
+		void Undo_last_message() {
 			   Messages.pop();
 			   set_Messages_in_file();
 		   }
-		   void Put_Message_Favorite(String^ message) {
+		void Put_Message_Favorite(String^ message) {
 			   favoritemessage.push(message);
 			   set_FavoriteMessages_in_file();
 		   }
-		   void Remove_theOldest_Message() {
+		void Remove_theOldest_Message() {
 			   favoritemessage.pop();
 			   set_FavoriteMessages_in_file();
 		   }
-		   void Search_About_Contact(String^ username) {
+		void Search_About_Contact(String^ username) {
 			   if (Contacts.find(username) != Contacts.end()) {
 				   Console::WriteLine(username + " " + Contacts[username]);
 			   }
@@ -192,7 +228,7 @@ namespace BlueSara7a {
 				   MessageBox::Show("This contact doesn't exist");
 			   }
 		   }
-		   bool Check(String^ username, long long id) {
+		bool Check(String^ username, long long id) {
 			   String^ filename = "check.txt";
 			   bool found = 0;
 			   try {
@@ -247,7 +283,7 @@ namespace BlueSara7a {
 			   }
 			   return found;
 		   }
-		   void Add_User(String^ username, long long id) {
+		void Add_User(String^ username, long long id) {
 			   if (!Check(username, id)) {
 				   return;
 			   }
@@ -293,14 +329,13 @@ namespace BlueSara7a {
 			   Add_panel->Hide();
 			   load_contacts();
 		   }
-		   void Display_Contacts() {
+		void Display_Contacts() {
 			   for (pair<String^, long long>pr : Contacts) {
 				   String^ last_message = get_last_message(pr.first, System::Convert::ToString(Contacts[pr.first]));
 				   Create_Contact(pr.first, last_message);
-				   Console::WriteLine(pr.first + " " + pr.second);
 			   }
 		   }
-		   String^ get_last_message(String^ name, String^ id) {
+		String^ get_last_message(String^ name, String^ id) {
 			   String^ last_message;
 			   String^ filename = "Data/";
 			   filename += username + "_" + System::Convert::ToString(ID) + "/" + id + ".txt";
@@ -323,22 +358,83 @@ namespace BlueSara7a {
 			   }
 			   return last_message;
 		   }
+		String^ get_username(long long id) {
+			   for (pair<String^, long long>pr : Contacts) {
+				   if (pr.second == id) {
+					   return pr.first;
+				   }
+			   }
+		   }
+		long long get_id(String^ name) {
+			return Contacts[name];
+		}
+		void Send_Message(String^ message, long long id) {
+			   String^ user = get_username(id);
+			   String^ filename = "Data/" + user + '_' + System::Convert::ToString(id) + '/' + System::Convert::ToString(ID) + ".txt";
+			   String^ filename1 = "Data/" + username + '_' + System::Convert::ToString(ID) + '/' + System::Convert::ToString(id) + ".txt";
+			   StreamWriter^ sw = gcnew StreamWriter(filename, true);
+			   sw->WriteLine("2" + message);
+			   sw->Close();
+			   StreamWriter^ sw1 = gcnew StreamWriter(filename1, true);
+			   sw1->WriteLine("1" + message);
+			   sw1->Close();
+			   Messages.push(message);
+			   set_Messages_in_file();
+		}
+		void Display_Chat()
+		{
+			String^ msg = "";
+			String^ msg2 = "";
+
+			while (!LifeChat.empty())
+			{
+				msg = LifeChat.front();
+				LifeChat.pop();
+				msg2 = "";
+				if (msg[0] == '2')
+				{
+					for (int i = 1; i < msg->Length; i++)
+					{
+						msg2 += msg[i];
+					}
+					Chat1->Items->Add(msg2);
+					Chat2->Items->Add("");
+				}
+				else if (msg[0]=='1')
+				{
+					for (int i = 1; i < msg->Length; i++)
+					{
+						msg2 += msg[i];
+					}
+					Chat2->Items->Add(msg2);
+					Chat1->Items->Add("");
+				}
+			}
+		}
+	private: System::Windows::Forms::ListBox^ Chat1;
+
+	private: System::Windows::Forms::Panel^ Add_panel;
+
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::TextBox^ Contact_ID;
+
+	private: System::Windows::Forms::TextBox^ Contact_Name;
+	private: System::Windows::Forms::Button^ Cancel_Button;
+
+
+	private: System::Windows::Forms::Button^ Add2_Button;
+	private: System::Windows::Forms::ListBox^ Chat2;
+
+
 	private: System::Windows::Forms::Button^ Search_button;
 	private: System::Windows::Forms::TextBox^ Search;
 	private: System::Windows::Forms::Button^ UnSearch_button;
 
-	private: System::Windows::Forms::Panel^ Add_panel;
-	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::TextBox^ Contact_ID;
-
-	private: System::Windows::Forms::TextBox^ Contact_Name;
-
-	private: System::Windows::Forms::Button^ Cancel_Button;
-	private: System::Windows::Forms::Button^ Add2_Button;
 
 	public:
 		void Create_Contact(String^ name, String^ last_message) {
+			String^ button_name = "Contact" + System::Convert::ToString(i);
 			Button^ Contact = gcnew Button;
 			Contact->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
@@ -439,14 +535,15 @@ namespace BlueSara7a {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(UserForm::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->Chat = (gcnew System::Windows::Forms::ListBox());
+			this->Chat2 = (gcnew System::Windows::Forms::ListBox());
 			this->Add_panel = (gcnew System::Windows::Forms::Panel());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->Contact_ID = (gcnew System::Windows::Forms::TextBox());
 			this->Contact_Name = (gcnew System::Windows::Forms::TextBox());
 			this->Cancel_Button = (gcnew System::Windows::Forms::Button());
 			this->Add2_Button = (gcnew System::Windows::Forms::Button());
+			this->Chat1 = (gcnew System::Windows::Forms::ListBox());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->Message = (gcnew System::Windows::Forms::TextBox());
 			this->panel2 = (gcnew System::Windows::Forms::Panel());
@@ -472,7 +569,9 @@ namespace BlueSara7a {
 			// panel1
 			// 
 			this->panel1->BackColor = System::Drawing::Color::Gray;
-			this->panel1->Controls->Add(this->Chat);
+			this->panel1->Controls->Add(this->Chat2);
+			this->panel1->Controls->Add(this->Add_panel);
+			this->panel1->Controls->Add(this->Chat1);
 			this->panel1->Controls->Add(this->button2);
 			this->panel1->Controls->Add(this->Message);
 			this->panel1->Location = System::Drawing::Point(396, 72);
@@ -480,53 +579,56 @@ namespace BlueSara7a {
 			this->panel1->Size = System::Drawing::Size(795, 602);
 			this->panel1->TabIndex = 1;
 			// 
-			// Chat
+			// Chat2
 			// 
-			this->Chat->FormattingEnabled = true;
-			this->Chat->ItemHeight = 16;
-			this->Chat->Location = System::Drawing::Point(7, 12);
-			this->Chat->Name = L"Chat";
-			this->Chat->Size = System::Drawing::Size(771, 516);
-			this->Chat->TabIndex = 9;
+			this->Chat2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Chat2->FormattingEnabled = true;
+			this->Chat2->ItemHeight = 29;
+			this->Chat2->Location = System::Drawing::Point(393, 12);
+			this->Chat2->Name = L"Chat2";
+			this->Chat2->RightToLeft = System::Windows::Forms::RightToLeft::Yes;
+			this->Chat2->Size = System::Drawing::Size(385, 497);
+			this->Chat2->TabIndex = 10;
 			// 
 			// Add_panel
 			// 
 			this->Add_panel->BackColor = System::Drawing::Color::Silver;
-			this->Add_panel->Controls->Add(this->label2);
-			this->Add_panel->Controls->Add(this->label1);
+			this->Add_panel->Controls->Add(this->label3);
+			this->Add_panel->Controls->Add(this->label4);
 			this->Add_panel->Controls->Add(this->Contact_ID);
 			this->Add_panel->Controls->Add(this->Contact_Name);
 			this->Add_panel->Controls->Add(this->Cancel_Button);
 			this->Add_panel->Controls->Add(this->Add2_Button);
-			this->Add_panel->Location = System::Drawing::Point(41, 202);
+			this->Add_panel->Location = System::Drawing::Point(21, 154);
 			this->Add_panel->Name = L"Add_panel";
 			this->Add_panel->Size = System::Drawing::Size(311, 194);
-			this->Add_panel->TabIndex = 8;
+			this->Add_panel->TabIndex = 9;
 			this->Add_panel->Hide();
 			// 
-			// label2
+			// label3
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->ForeColor = System::Drawing::Color::Black;
-			this->label2->Location = System::Drawing::Point(44, 89);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(28, 20);
-			this->label2->TabIndex = 5;
-			this->label2->Text = L"ID";
+			this->label3->ForeColor = System::Drawing::Color::Black;
+			this->label3->Location = System::Drawing::Point(44, 89);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(28, 20);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"ID";
 			// 
-			// label1
+			// label4
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->ForeColor = System::Drawing::Color::Black;
-			this->label1->Location = System::Drawing::Point(21, 40);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(57, 20);
-			this->label1->TabIndex = 4;
-			this->label1->Text = L"Name";
+			this->label4->ForeColor = System::Drawing::Color::Black;
+			this->label4->Location = System::Drawing::Point(21, 40);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(57, 20);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"Name";
 			// 
 			// Contact_ID
 			// 
@@ -570,6 +672,17 @@ namespace BlueSara7a {
 			this->Add2_Button->UseVisualStyleBackColor = false;
 			this->Add2_Button->Click += gcnew System::EventHandler(this, &UserForm::Add2_Button_Click);
 			// 
+			// Chat1
+			// 
+			this->Chat1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Chat1->FormattingEnabled = true;
+			this->Chat1->ItemHeight = 29;
+			this->Chat1->Location = System::Drawing::Point(7, 12);
+			this->Chat1->Name = L"Chat1";
+			this->Chat1->Size = System::Drawing::Size(385, 497);
+			this->Chat1->TabIndex = 9;
+			// 
 			// button2
 			// 
 			this->button2->BackColor = System::Drawing::Color::Transparent;
@@ -584,6 +697,9 @@ namespace BlueSara7a {
 			this->button2->Size = System::Drawing::Size(46, 41);
 			this->button2->TabIndex = 1;
 			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &UserForm::button2_Click);
+			this->button2->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &UserForm::button2_KeyDown);
+			//this->button2->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &UserForm::button2_KeyPress);
 			// 
 			// Message
 			// 
@@ -745,7 +861,6 @@ namespace BlueSara7a {
 			this->panel4->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
 				static_cast<System::Int32>(static_cast<System::Byte>(64)));
 			this->panel4->Controls->Add(this->UnSearch_button);
-			this->panel4->Controls->Add(this->Add_panel);
 			this->panel4->Controls->Add(this->Search_button);
 			this->panel4->Controls->Add(this->Search);
 			this->panel4->Location = System::Drawing::Point(0, 75);
@@ -831,6 +946,7 @@ namespace BlueSara7a {
 
 		}
 #pragma endregion
+	
 
 	private: System::Void Logout_Button_Click(System::Object^ sender, System::EventArgs^ e) {
 		this->Hide();
@@ -854,7 +970,17 @@ namespace BlueSara7a {
 		Star_Button->Show();
 	}
 	private: System::Void Contact_Click(System::Object^ sender, System::EventArgs^ e) {
-
+		Button^ this_Button = (Button^)sender;
+		name = "";
+		for (int i = 0; i < this_Button->Text->Length; i++) {
+			if (this_Button->Text[i] != '\n') {
+				name += this_Button->Text[i];
+			}
+			else { break; }
+		}
+		long long id = get_id(name);
+		load_messages(id);
+		Message->Focus();
 	}
 	private: System::Void Star2_button_Click(System::Object^ sender, System::EventArgs^ e) {
 		Star2_button->Hide();
@@ -882,6 +1008,21 @@ namespace BlueSara7a {
 	private: System::Void Add2_Button_Click(System::Object^ sender, System::EventArgs^ e) {
 		Add_User(Contact_Name->Text, System::Convert::ToInt32(Contact_ID->Text));
 	}
-	};
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ msg = Message->Text;
+		Chat2->Items->Add(msg);
+		Chat1->Items->Add("");
+		//Send_Message(msg, get_id(name));
+		//load_messages(get_id(name));
+		Message->Clear();
+		Message->Focus();
+	}		  
+	private: System::Void button2_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+		if (e->KeyValue == (int)Keys::Enter)
+		{
+			button2->PerformClick();
+		}
+	}
 
+};
 }
